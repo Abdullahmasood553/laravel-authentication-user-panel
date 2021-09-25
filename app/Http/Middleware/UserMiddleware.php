27206487f27cf759b65ecce3388e3817e649937c
Abdullah Masood <amasood@octans.ai>
2021-09-25 22:10:41 +0500

    Authentication User Panel

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Auth;
 
 class UserMiddleware
 {
@@ -16,6 +17,10 @@ class UserMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+            if(!Auth::check()) {
+                $user = Auth::user();
+                return redirect()->route('login');
+            }
+            return $next($request);
     }
 }
